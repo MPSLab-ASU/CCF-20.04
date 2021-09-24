5f2c8bc0fcd0b4b59a263992775796f8c7c9371d
Mahesh Balasubramanian (Student) <mbalasu2@en4181851l.fulton.ad.asu.edu>
2021-09-23 18:38:58 -0700

    pow benchmark added

--- benchmarks/pow/pow.c
+++ benchmarks/pow/pow.c
@@ -12,20 +12,26 @@
   return ret;
   }*/
 
-int power(int base, int pow){
+/*int power(int base, int pow){
   if(pow == 0) return 1;
   printf("Base = %d - Pow = %d\n", base, pow);
   int ret = base;
   #pragma CGRA
-  for(int i = 1; i < pow - 1; i++)
+  for(int i = 1; i < pow; i++)
     ret *= base;
   return ret;
-}
+}*/
 
 int main(int argc, char *argv[])
 {
   int pow = atoi(argv[1]);
-  int ret = power(2, pow);
+  int base = 2;
+  if(pow == 0) return 1;
+  printf("Base = %d - Pow = %d\n", base, pow);
+  int ret = base;
+  #pragma CGRA
+  for(int i = 1; i < pow; i++)
+    ret *= base;
   //int ret = sum(pow);
   
   printf("***** %d^%d = %d *****\n", 2, pow, ret);
